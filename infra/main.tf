b769b1cc2082780e362d84d9533cf92cd4bcfdf6
Radosław Winiecki <r.winiecki@outlook.com>
2025-07-21 18:32:58 +0200

    allow gh actions to auth aws

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,59 @@
 provider "aws" {
-    region = "eu-west-1"  
+  region = "eu-west-1"
 }
 
 resource "aws_s3_bucket" "spotify_auth_cache" {
-  bucket = "radek-spotify-auth-cache"
+  bucket        = "radswn-spotify-auth-cache"
   force_destroy = true
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          "token.actions.githubusercontent.com:sub" = "repo:radswn/crate-digger:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "s3_access" {
+  name = "allow-token-s3"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket"
+      ],
+      Resource = aws_s3_bucket.spotify_auth_cache.arn
+    }]
+  })
 }
