0e7271563ff505816f47fbe7032e5045d168e13d
Radosław Winiecki <r.winiecki@outlook.com>
2025-07-24 15:44:25 +0200

    format notification message

--- src/crate_digger/utils/telegram.py
+++ src/crate_digger/utils/telegram.py
@@ -26,22 +26,25 @@ def send_message(message: str) -> None:
 
 
 def construct_message(releases_info: Dict[str, Dict[str, List[Dict]]]) -> str:
-    message_text = bold("NEW RELEASES") + "\n"
+    n_tracks_found = sum([len(tracks) for release in releases_info.values() for tracks in release.values()])
+
+    message_text = "❗" + bold("NEW RELEASES") + "❗" + "\n\n"
+    message_text += "🎵 FOUND " + bold(str(n_tracks_found)) + " TRACKS 🎵" + "\n"
 
     for label, release_titles in releases_info.items():
-        message_text += "\n\n" + escape_markdown_v2(label) + "\n"
+        message_text += "\n\n" + "🎤 " + escape_markdown_v2(label).upper() + "\n"
 
         for release, tracks in release_titles.items():
-            message_text += "\n" + escape_markdown_v2(release) + "\n"
+            message_text += "\n" + "💿 " +  escape_markdown_v2(release) + "\n"
 
             for i, track in enumerate(tracks):
                 joined_artists = ", ".join([artist["name"] for artist in track["artists"]])
                 title = track["name"]
 
-                track_line = escape_markdown_v2(f"{i + 1}. {joined_artists} - {title}\n")
+                track_line = escape_markdown_v2(f"{i + 1}. {joined_artists} - {title}")
                 if track["is_added"]:
                     track_line = underline(track_line)
-                message_text += track_line
+                message_text += track_line + "\n"
 
     return message_text
 
