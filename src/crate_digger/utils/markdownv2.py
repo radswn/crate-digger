85218d92887b9989fe2ffb30162ab5349a61e7be
Radosław Winiecki <r.winiecki@outlook.com>
2025-07-26 20:45:10 +0200

    remove unused functions

--- src/crate_digger/utils/markdownv2.py
+++ src/crate_digger/utils/markdownv2.py
@@ -2,13 +2,5 @@ def bold(text: str) -> str:
     return f"*{text}*"
 
 
-def italic(text: str) -> str:
-    return f"_{text}_"
-
-
 def underline(text: str) -> str:
     return f"__{text}__"
-
-
-def strikethrough(text: str) -> str:
-    return f"~{text}~"
