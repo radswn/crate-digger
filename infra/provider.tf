819b1041cb152e0a00a816010a59c8d26e6bd1ba
Radosław Winiecki <r.winiecki@outlook.com>
2026-01-04 11:56:58 +0100

    fix infra and add s3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket       = "radswn-tf-state-bucket"
+    key          = "crate-digger/infra/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
